c3a3eae9b2d65af4d37aaef6a7ebb9682ccf48c1
Brayan Espinoza <brayan.espinozaa97@gmail.com>
2023-12-09 11:27:08 -0600

    steps styled

--- app/components/Steps.js
+++ app/components/Steps.js
@@ -1,19 +1,29 @@
 import React from 'react'
+import styles from './Steps.module.css'
+import { AiOutlineFileSync } from "react-icons/ai";
+import { GrDocumentDownload } from "react-icons/gr";
+import { LuFileSignature } from "react-icons/lu";
 
 export const Steps = () => {
   return (
-    <div>
-        <div>
-            <div>Paso 1</div>
-            <div>Agrega tu perfil</div>
+    <div className={styles.container}>
+
+        <div className={styles.item}>
+        <LuFileSignature className={styles['item-icon']}/>
+            <div className={styles['item-title']}>Paso 1</div>
+            <div className={styles['item-description']}>Agrega tu FIEL</div>
         </div>
-        <div>
-            <div>Paso 2</div>
-            <div>Sincroniza tus cuentas bancarias</div>
+
+        <div className={styles.item}>
+        <AiOutlineFileSync className={styles['item-icon']}/>
+            <div className={styles['item-title']}>Paso 2</div>
+            <div className={styles['item-description']}>Sincroniza tus cuentas bancarias</div>
         </div>
-        <div>
-            <div>Paso 3</div>
-            <div>Recibe mensualmente tu declaracion de impuestos</div>
+        
+        <div className={styles.item}>
+        <GrDocumentDownload className={styles['item-icon']}/>
+            <div className={styles['item-title']}>Paso 3</div>
+            <div className={styles['item-description']}>Recibe mensualmente tu declaracion de impuestos</div>
         </div>
     </div>
   )
