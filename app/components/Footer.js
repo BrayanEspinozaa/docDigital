e85c430ddabdd021e9c699770ec951d45fb5b893
Brayan Espinoza <brayan.espinozaa97@gmail.com>
2023-12-09 11:47:24 -0600

    footer styled

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,16 +1,20 @@
 import { Button } from 'antd'
 import React from 'react'
+import styles from './Footer.module.css'
 
 export const Footer = () => {
-  return (
-    <div>
-        <p>
-            Quieres cambiar a tu conta? Nosotros no encargamos de llevar a cabo la transicion de toda tu contabilidad hasta el dia de hoy.
-        </p>
+    return (
+        <div className={styles.container}>
+            <p>
+                Quieres cambiar a tu conta? Nosotros no encargamos de llevar a cabo la transicion de toda tu contabilidad hasta el dia de hoy.
+            </p>
 
-        <Button>Ver Paquetes</Button>
-        <Button>Agenda una cita con un contador experto</Button>
+            <div className={styles['buttons-container']}>
 
-    </div>
-  )
+                <button className={styles['button-primary']}>Ver Paquetes</button>
+                <button className={styles['button-secundary']}>Agenda una cita con un contador experto</button>
+            </div>
+
+        </div>
+    )
 }
