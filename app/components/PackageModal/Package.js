eb27827f53f396a24c1c5bf913a31adcd7eee78e
Brayan Espinoza <brayan.espinozaa97@gmail.com>
2023-12-10 09:52:50 -0600

    refactor package data

--- app/components/PackageModal/Package.js
+++ app/components/PackageModal/Package.js
@@ -1,56 +1,34 @@
 import React from 'react'
 import styles from './Package.module.css'
+import { packages } from '../utils/constants'
 
 export const Package = () => {
     return (
         <div className={styles.container}>
-            <div className={styles.item}>
-                <div className={styles['item-title']}>Emprendedor</div>
-                <div className={styles['item-content-primary']}>
-                    $ <span>1,000</span> + IVA/mes
-                </div>
-                <div className={styles['item-content-secundary']}>
-                    1 - 30 movimientos bancarios al mes
-                </div>
 
-                <button>Elegir</button>
-            </div>
-            <div className={styles.item}>
-                <div className={styles['item-title']}>Emprendedor Plus</div>
-                <div className={styles['item-content-primary']}>
-                    $ <span>2,000</span> + IVA/mes
-                </div>
-                <div className={styles['item-content-secundary']}>
-                    31 - 100 movimientos bancarios al mes
-                </div>
-
-                <button>Elegir</button>
-            </div>
-            <div className={styles.item}>
-                <div className={styles['item-title']}>Empresarial</div>
-                <div className={styles['item-content-primary']}>
-                    $ <span>3,000</span> + IVA/mes
-                </div>
-                <div className={styles['item-content-secundary']}>
-                    101 - 300 movimientos bancarios al mes
-                </div>
-
-                <button>Elegir</button>
-            </div>
-            <div className={styles.item}>
-                <div className={styles['item-title']}>Select</div>
-                <div className={styles['item-content-primary-bolder']}>
-                    Contactanos para hacer un paquete a tus necesidades
-                </div>
-                <div className={styles['item-content-secundary']}>
-
-                    + 300 movimientos bancarios al mes
-                </div>
-
-                <button>Cotizar paquete</button>
-            </div>
-
-
+            {
+                packages.map(item => (
+                    <div key={item.key} className={styles.item}>
+                        <div className={styles['item-title']}>{item.title}</div>
+                        {
+                            item.price
+                                ?
+                                <div className={styles['item-content-primary']}>
+                                    $ <span>{item.price}</span> + IVA/mes
+                                </div>
+                                :
+                                <div className={styles['item-content-primary-bolder']}>
+                                    Contactanos para hacer un paquete a tus necesidades
+                                </div>
+                        }
+                        {
+                            item.price
+                                ? <button>Elegir</button>
+                                : <button>Cotizar paquete</button>
+                        }
+                    </div>
+                ))
+            }
         </div>
     )
 }
---
+++ app/components/utils/constants.js
@@ -0,0 +1,25 @@
+export const packages = [
+    {
+        id: 1,
+        title: 'Emprendedor',
+        price: '1,000',
+        movements: '1 - 30'
+    },
+    {
+        id: 1,
+        title: 'Emprendedor Plus',
+        price: '2,000',
+        movements: '31 - 100'
+    },
+    {
+        id: 1,
+        title: 'Empresarial',
+        price: '3,000',
+        movements: '101 - 300'
+    },
+    {
+        id: 1,
+        title: 'Emprendedor',
+        movements: '+ 300'
+    },
+]
